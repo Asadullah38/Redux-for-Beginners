603237647ccf0f0b32829aa4a64b549b7ae02603
Asadullah38 <2020se10@student.uet.edu.pk>
2022-09-14 18:44:00 +0500

    Action-creator and Reducer Folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-
+import React from "react";
+import "./App.css";
 const App = () => {
   return (
-    <div><h1>Redux First Tutorial Project</h1>
-    <button>Balance is 0</button></div>
-  )
-}
+    <div>
+      <h1>Redux First Tutorial Project</h1>
+      <button id="button">+</button>
+      <button id="button">-</button>
+      
+    </div>
+  );
+};
 
 export default App;
