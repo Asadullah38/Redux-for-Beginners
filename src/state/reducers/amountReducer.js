c78d7f42a8b9d695b87ef3757e6b6257c2853a44
Asadullah38 <2020se10@student.uet.edu.pk>
2022-09-15 01:31:50 +0500

    Final Version

--- src/state/reducers/amountReducer.js
+++ src/state/reducers/amountReducer.js
@@ -2,7 +2,7 @@ const reducer = (state = 0, action) => {
   if (action.type === "deposit") {
     return state + action.payload;
   } else if (action.type === "withdraw") {
-    return state - action.payload;
+    return state - action.payload >= 0 ? state - action.payload : state;
   } else {
     return state;
   }
