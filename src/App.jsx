2f061448b7674addfab6da9ca24d767276f75d93
Asadullah38 <2020se10@student.uet.edu.pk>
2022-09-15 00:13:47 +0500

    Setting up Store

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import "./App.css";
 const App = () => {
+  const amount=useSelector(state=>state.amount);
   return (
     <div>
       <h1>Redux First Tutorial Project</h1>
       <button id="button">+</button>
       <button id="button">-</button>
+      <h1>Your Balance is {amount}</h1>
       
     </div>
   );
